4a5b7de4e0f0de10385e199914b0b16033ee8be5
Pauline Ramelet <pauline.ramelet@icloud.com>
2025-12-02 13:07:11 +0100

    Ajout des fichiers styles.css functions.php index.php single.php et page.php

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+// Charger le style principal
+function motaphoto_enqueue_styles() {
+    wp_enqueue_style('motaphoto-style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'motaphoto_enqueue_styles');
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<main>
+    <h1>Titre temporaire principal de Mota Photo - pour création index.php</h1>
+
+</main>
+
+<?php get_footer(); ?>
