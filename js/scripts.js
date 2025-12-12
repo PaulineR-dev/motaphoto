7687a50971de82f32d2d058ca69141e6f7546109
Pauline Ramelet <pauline.ramelet@icloud.com>
2025-12-12 13:02:05 +0100

    Modifications JS pour les animations menus et modale

--- js/scripts.js
+++ js/scripts.js
@@ -1,168 +1,167 @@
 document.addEventListener('DOMContentLoaded', function () { // Chargement du script après que tout le DOM soit chargé
   // Récupération des éléments nécessaires
-  const modale = document.getElementById('contact-modale');
+  const contactModale = document.getElementById('contact-modale');
   const contactLink = document.querySelector('.modale-contact');
   const menuToggle = document.querySelector('.menu-toggle'); 
   const mobileHeaderOverlay = document.getElementById('mobile-header-overlay');
-  const closeBtn = mobileHeaderOverlay.querySelector('.overlay-close');
-  const body = document.body;
+  const closeBtn = document.querySelector('.overlay-close');
+  const menuHeader = document.getElementById('menu-motaphoto_menu');
+  // const body = document.body;
 
   // Vérifie si on est en version mobile ou pas
-  function isMobile() {
+  function ecranMobile() {
     return window.matchMedia("(max-width: 768px)").matches;
   }
 
+
   // ANIMATIONS POUR VERSION DESKTOP
+
   // Animation d'apparition Fade in (desktop)
-  function fadeIn(element, duration = 300) {
-    element.style.display = 'flex';
-    element.animate([
-      { opacity: 0, transform: 'none' }, // Départ : invisible
-      { opacity: 1, transform: 'none' } // Arrivée : visible
+  function fadeInContact() {
+    contactModale.style.display = 'flex';
+    contactModale.animate([
+      { opacity: 0, transform: 'none' },
+      { opacity: 1, transform: 'none' }
     ], {
-      duration: duration,
+      duration: 300,
       easing: 'ease-out',
-      fill: 'forwards'
+      fill: 'forwards' // L'élément conserve l'état final à la fin de l'animation
     });
   }
 
-  // Fade out (desktop)
-  function fadeOut(element, duration = 300) {
-    const animation = element.animate([
-      { opacity: 1, transform: 'none' }, // Départ : visible
-      { opacity: 0, transform: 'none' } // Arrivée : invisible
+  // Animation de disparition Fade out (desktop)
+  function fadeOutContact() {
+    const animationFadeOutContact  = contactModale.animate([
+      { opacity: 1, transform: 'none' },
+      { opacity: 0, transform: 'none' }
     ], {
-      duration: duration,
+      duration: 300,
       easing: 'ease-out',
       fill: 'forwards'
     });
 
-    animation.onfinish = () => {
-      element.style.display = 'none';
+    animationFadeOutContact.onfinish = () => {
+      contactModale.style.display = 'none';
     };
   }
 
-  // ANIMATIONS VERSION MOBILE
+
+  // ANIMATIONS POUR VERSION MOBILE
+
   // Animation d'apparition de droite à gauche pour le menu mobile
-  function fadeSlideIn(element, duration = 300) {
-    element.style.display = 'flex'; // Rend visible
-    element.animate([
-      { transform: 'translateX(100%)', opacity: 0 }, // Départ : hors écran, invisible
-      { transform: 'translateX(0)', opacity: 1 } // Arrivée : en place, visible
+  function showMenuMobile() {
+    if (!mobileHeaderOverlay) return;
+    mobileHeaderOverlay.style.display = 'flex';
+    mobileHeaderOverlay.animate([
+      { transform: 'translateX(100%)', opacity: 0, backgroundColor: 'transparent' },
+      { transform: 'translateX(0)', opacity: 1, backgroundColor: '#fff' }
     ], {
-      duration: duration, // Durée de 300ms comme requis par la maquette Figma
-      easing: 'ease-out', // Courbe d'animation comme requis sur Figma : démarre vite et ralentit progressivement
-      fill: 'forwards' // Conserve l'état final
+      duration: 300,
+      easing: 'ease-out',
+      fill: 'forwards'
     });
   }
 
   // Animation de disparition de gauche à droite pour le menu mobile
-  function fadeSlideOut(element, duration = 300) {
-    const animation = element.animate([
-      { transform: 'translateX(0)', opacity: 1 }, // Départ : en place, visible
-      { transform: 'translateX(100%)', opacity: 0 } // Arrivée : hors écran, invisible
+  function hideMenuMobile() {
+    if (!mobileHeaderOverlay) return;
+    const animationHideMenuMobile = mobileHeaderOverlay.animate([
+      { transform: 'translateX(0)', opacity: 1, backgroundColor: '#fff' },
+      { transform: 'translateX(100%)', opacity: 0, backgroundColor: 'transparent' }
     ], {
-      duration: duration,
+      duration: 300,
       easing: 'ease-out',
       fill: 'forwards'
     });
 
-    // Quand l'animation est terminée, on cache l'élément
-    animation.onfinish = () => {
-      element.style.display = 'none';
+    animationHideMenuMobile.onfinish = () => {
+      mobileHeaderOverlay.style.display = 'none';
     };
   }
 
-  // Gestion des événements
-  // Ouverture de la modale au clic sur "Contact"
-  if (contactLink && modale) { // Vérifie que le lien "Contact" et la modale existent bien dans le DOM
-
-    // Ajoute un écouteur d'événement sur le lien "Contact"
-    contactLink.addEventListener('click', function (event) {
-      event.preventDefault(); // Empêche le comportement par défaut du lien
-      
-      // Vérifie si version mobile ou pas
-      if (isMobile()) {
-
-        // Sur mobile : ouverture de la modale avec animation "slide in"
-        fadeSlideIn(modale, 300);
-        // Vérifie si le menu burger existe
-        if (menuToggle) {
-          // Ferme le menu burger instantanément sans animation
-          body.classList.add('no-transition'); // Ajoute une classe qui désactive les transitions
-          menuToggle.checked = false; // Décoche le bouton burger donc ferme le menu
-          // Retire la classe juste après pour rétablir les transitions normales après 50ms
-          setTimeout(() => body.classList.remove('no-transition'), 50);
-        }
-      } else {
-        // Sinon (donc desktop) : ouverture de la modale avec l'animation fade in
-        fadeIn(modale, 300);
-      }
+  // Animation d'apparition de droite à gauche pour la modale Contact (mobile)
+  function fadeSlideInContact() {
+    contactModale.style.display = 'flex';
+    contactModale.animate([
+      { transform: 'translateX(100%)', opacity: 0 },
+      { transform: 'translateX(0)', opacity: 1 }
+    ], {
+      duration: 300, // Comme défini sur maquette Figma
+      easing: 'ease-out', // Comme défini sur maquette Figma
+      fill: 'forwards'
     });
   }
 
-  // Ferme la modale en cliquant sur l’overlay
+  // Animation de disparition de gauche à droite pour la modale Contact (mobile)
+  function fadeSlideOutContact() {
+    const animationfadeSlideOutContact = contactModale.animate([
+      { transform: 'translateX(0)', opacity: 1 },
+      { transform: 'translateX(100%)', opacity: 0 }
+    ], {
+      duration: 300,
+      easing: 'ease-out',
+      fill: 'forwards'
+    });
+
+    animationfadeSlideOutContact.onfinish = () => {
+      contactModale.style.display = 'none';
+    };
+  }
+
+
+  // GESTION DES EVENEMENTS
+
+  // Ouverture de la modale au clic sur "Contact"
+  contactLink.addEventListener('click', function (eventClicContact) {
+    eventClicContact.preventDefault();
+
+    if (ecranMobile()) {
+      fadeSlideInContact();
+        menuHeader.style.transition = 'none'; // Désactive les transitions du menu
+        menuToggle.checked = false; // Ferme le menu burger
+        mobileHeaderOverlay.style.display = 'none'; // Cache le menu overlay mobile sans transition
+      // Réactive les transitions après un court délai
+      setTimeout(() => {
+        menuHeader.style.transition = ''; // Retour aux transitions CSS normales
+      }, 50);
+
+    } else {
+      fadeInContact(); // Desktop
+    }
+  });
+
+  // }
+
+  // Fermeture de la modale en cliquant sur l’overlay
   window.addEventListener('click', function (event) {
-    if (modale && event.target === modale) { // Vérifie que le clic s'effectue bien sur l'overlay
-      if (isMobile()) {
-        fadeSlideOut(modale, 300); // Animation mobile : slide out
+    if (contactModale && event.target === contactModale) {
+      if (ecranMobile()) {
+        fadeSlideOutContact();
       } else {
-        fadeOut(modale, 300); // Animation desktop : fade out
+        fadeOutContact();
       }
     }
   });
 
-  // Menu qui se superpose et qui coulisse en mobile
-  function showMenuOverlay(element, duration = 300) {
-    if (!element) return;
-    element.style.display = 'flex';
-    element.animate([
-      { transform: 'translateX(100%)', opacity: 0, backgroundColor: 'transparent' }, 
-      { transform: 'translateX(0)', opacity: 1, backgroundColor: '#fff' }
-    ], {
-      duration: duration,
-      easing: 'ease-out',
-      fill: 'forwards'
-    });
-  }
-
-  // Cache l'overlay mobile avec coulissement et retour transparent
-  function hideMenuOverlay(element, duration = 300) {
-    if (!element) return;
-      const animation = element.animate([
-        { transform: 'translateX(0)', opacity: 1, backgroundColor: '#fff' },
-        { transform: 'translateX(100%)', opacity: 0, backgroundColor: 'transparent' }
-    ], {
-      duration: duration,
-      easing: 'ease-out',
-      fill: 'forwards'
-    });
-
-    animation.onfinish = () => {
-      element.style.display = 'none';
-    };
-  }
-
-  // Ouvrir/fermer via le burger
+  // Ouverture et fermeture du menu burger
   menuToggle.addEventListener('change', () => {
     if (menuToggle.checked) {
-      showMenuOverlay(mobileHeaderOverlay);
+      showMenuMobile();
     } else {
-      hideMenuOverlay(mobileHeaderOverlay);
+      hideMenuMobile();
     }
   });
 
-  // Fermer via la croix
+  // Fermeture du menu burger au clic sur la croix
   closeBtn.addEventListener('click', () => {
     menuToggle.checked = false;
-    hideMenuOverlay(mobileHeaderOverlay);
-    menuToggle.focus();
+    hideMenuMobile();
   });
 
-  // Referme si on passe en desktop
+  // Fermeture du menu burger si passe en desktop
   window.addEventListener('resize', () => {
     if (window.matchMedia("(min-width: 769px)").matches) {
-      hideMenuOverlay(mobileHeaderOverlay);
+      hideMenuMobile();
       menuToggle.checked = false;
     }
   });
